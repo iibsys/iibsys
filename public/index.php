89cba7751b592d044d72158b0d5e85ed40c20592
Zeeshan Abbas <zeeshan@iibsys.com>
2017-03-27 15:24:36 +0300

    The IIBSys application structure

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * index.php
+ *
+ * Redirects to the public system.
+ *
+ * @copyright 
+ * @license 
+ * @version 
+ * @package 
+ */
+
+// The Application's Public Domain.
+
+
+
+// If we are, go ahead to the public domain
+header('Location: ../public/');
